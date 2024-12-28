30a7acbf77f96d0af4a6a103c035ae638a921155
Ankit Jha <ankitjhacse2003@gmail.com>
2024-12-28 17:13:19 +0530

    first commit

---
+++ arrays.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+//creation
+
+// int arr[10];
+// int crr[]={10,20,40,70};
+
+// int drr[4]={0}; 
+// cout<<drr[0]<<endl;
+// cout<<crr[1]<<endl;
+ int arr[4];
+fill(arr,arr+4,23);
+cout<<arr[0] <<"" << arr[1] <<"" << arr[2] <<"" << arr[3] <<endl;
+
+return 0;
+}
