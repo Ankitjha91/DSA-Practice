1970c7cb28c59ddf53a80a89818d534a1419a33f
Ankit Jha <ankitjhacse2003@gmail.com>
2025-01-27 20:42:26 +0530

    code push

--- Question6.cpp
+++ Question6.cpp
@@ -20,7 +20,7 @@ pair<int, int> countOddEven(vector<int>& arr) {
 }
 
 int main() {
-   vector<int> arr = {2, 3, 4, 5};
+   vector<int> arr = {2, 3, 4, 5,6,7,8,9};
    pair<int, int> ans = countOddEven(arr);
    cout << ans.first << " " << ans.second;
    return 0;
