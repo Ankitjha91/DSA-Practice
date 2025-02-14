8ad7d7441a61ad900a068363565bd78c6f1a4d96
Ankit Jha <ankitjhacse2003@gmail.com>
2025-02-14 22:50:11 +0530

    ASCII value check

---
+++ Question13.cpp
@@ -0,0 +1,21 @@
+// C++ program to print
+// ASCII Value of Character
+// #include <iostream>
+// using namespace std;
+
+// Driver code
+// int main()
+// {
+//     char c = 'A';
+//     cout << "The ASCII value of " << 
+//              c << " is " << int(c);
+//     return 0;
+// }
+#include<iostream>
+using namespace std;
+int main(){
+    char c = 'A';
+    cout<<" the ASCII value of "<<c<< "is "<<int (c);
+    return 0;
+
+}
